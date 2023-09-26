e49a907312953d1961e6f93fefeeca050c9283f3
Lunatik4872 <gael.pailly@gmail.com>
2023-09-26 22:59:00 +0200

    LesGaulois TP1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,56 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+		}
+
+	private String prendreParole() {
+		
+		return "Le gaulois" + nom + " : " ;
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup((force / 3)*effetPotion);
+	}
+	
+	public void boirepotion(int power) {
+		
+		effetPotion = power;
+		if (power==3) {
+			parler("Merci Druide, je sens que ma force est 3 fois decuplee");
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix",8);
+		Romain bernard = new Romain("Bernard",12);
+		asterix.boirepotion(10);
+		asterix.boirepotion(3);
+		asterix.frapper(bernard);
+		asterix.frapper(bernard);
+		
+	}
+
+}
