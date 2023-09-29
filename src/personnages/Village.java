89fe9bfaffd60225e34490c541e6a37423c17a22
Lunatik4872 <gael.pailly@gmail.com>
2023-09-29 08:07:52 +0200

    maj

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,5 +1,7 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Village {
 	
 	
@@ -27,15 +29,37 @@ public class Village {
 	}
 	
 	public Gaulois trouverHabitant(int numeroVillageois) {
-		return villageois[numeroVillageois];
+		return villageois[numeroVillageois-1];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans " + nom + " du chef " + chef.getNom() + " vivent les legendaires gaulois :");
+		for (int i = 0; i < nb_villageois; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+		
 	}
 	
 	public static void main(String[] args) {
-		Village village = new Village("Village des Irréductibles", 30);
-		//Gaulois gaulois = village.trouverHabitant(30);
-		// On créer un tableau à 30 positions mais dans un tableau de taille n
-		// les indices vont de 0 à n-1
+		Village village = new Village("Village des Irreductibles", 30);
 		
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		 On créer un tableau à 30 positions avce des null donc pas du bon type
+		
+		Chef abraracourcix = new Chef("Abraracourcix",6,1,village);
+		village.setChef(abraracourcix);
+		
+		Gaulois asterix = new Gaulois("Asterix",8);
+		village.ajouterHabitant(asterix);
+		Gaulois obelix = new Gaulois("Obelix",25);
+		village.ajouterHabitant(obelix);
+		
+		village.afficherVillageois();
+
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//      On retrouve bien le gaulois ajouté au village car désormais ce n'est plus null mais de type Gaulois
+
 
 	}
 
